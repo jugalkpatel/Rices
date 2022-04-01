60736bb63401ece0a23726c755a972a431624692
jugalkpatel <ptjimmy99@gmail.com>
2022-04-02 02:14:25 +0530

    feat: add container in the Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,36 @@
+import { Stack, Container, createStyles } from "@mantine/core";
 import { NavigationBar } from "@/components/all";
 
 type Props = {
   children: JSX.Element | JSX.Element[];
 };
 
+const useStyles = createStyles((theme) => ({
+  width: {
+    width: "100%",
+
+    [theme.fn.largerThan("md")]: {
+      width: "720px",
+    },
+
+    [theme.fn.largerThan("lg")]: {
+      width: "960px",
+    },
+
+    [theme.fn.largerThan("xl")]: {
+      width: "1140px",
+    },
+  },
+}));
+
 function Layout({ children }: Props) {
+  const { classes } = useStyles();
   return (
     <>
       <NavigationBar />
-      {children}
+      <Stack justify="center">
+        <Container className={classes.width}>{children}</Container>
+      </Stack>
     </>
   );
 }
